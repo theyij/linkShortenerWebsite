6e468da18eeb266fff0617ed6ee9305ae8e6e352
ijen <48159633+theyij@users.noreply.github.com>
2021-10-14 22:23:11 +0900

    Manipulate html with input value

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-//get input data
+// Select input element id -> get its value
+let getInputValue = function () {
+        let inputVal =  document.getElementById("url").value;
+
+        // Manipulate html with input value
+        document.getElementById("shortUrl").innerText = inputVal;
+      }
 
 //send to bitly API
 
